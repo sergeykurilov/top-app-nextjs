6419932287c1f598b8b3608051e097c0e2eabeac
Sergey <kurilovsergey15@gmail.com>
2021-12-27 21:19:56 +0200

    feat: 111

--- components/P/P.tsx
+++ components/P/P.tsx
@@ -3,7 +3,7 @@ import cn from 'classnames';
 import styles from './P.module.css';
 
 export const P = ({size = 'small' ,children, className, ...props}: PProps): JSX.Element => {
-
+    //
     return (
         <p
             className={cn(styles.p, className, {
