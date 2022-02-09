05a86394daab03de3ca83222cedf64dbbecb2822
Sergey <kurilovsergey15@gmail.com>
2022-02-09 20:18:59 +0200

    feat: 111

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -4,6 +4,7 @@ import cn from "classnames";
 import {format} from "date-fns";
 
 export const Footer = ({className,...props}: FooterProps): JSX.Element => {
+    ///
     return (
         <footer className={cn(className, styles.footer)} {...props} >
             <div className={styles.allRightsClass}>OwlTop © {format(new Date, 'yyyy')} Все права защищены</div>
